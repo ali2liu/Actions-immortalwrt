0399d17abc2f2c19ca0790588c58d62b29b72ae0
ali2liu <ali2liu@gmail.com>
2025-03-16 08:03:00 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -3,9 +3,9 @@
 #删除feeds中的插件
 # rm -rf ./feeds/packages/net/mosdns
 # rm -rf ./feeds/packages/net/v2ray-geodata
-rm -rf ./feeds/packages/net/geoview
-rm -rf ./feeds/packages/net/shadowsocks-libev
-rm -rf ./feeds/packages/net/chinadns-ng
+# rm -rf ./feeds/packages/net/geoview
+# rm -rf ./feeds/packages/net/shadowsocks-libev
+# rm -rf ./feeds/packages/net/chinadns-ng
 
 
 #克隆依赖插件
