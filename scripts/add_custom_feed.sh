8c79ef566d4b4fbfe2d68179bbc353caea61ec83
ali2liu <ali2liu@gmail.com>
2025-12-17 11:10:10 +0800

    Add custom feeds to feeds.conf.default

--- scripts/add_custom_feed.sh
+++ scripts/add_custom_feed.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
