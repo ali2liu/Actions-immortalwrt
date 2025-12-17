a05f01aa476a6d5540b31da50c2a480e5eb29745
ali2liu <ali2liu@gmail.com>
2025-12-17 11:00:47 +0800

    Update packages.sh

--- scripts/packages.sh
+++ scripts/packages.sh
@@ -7,8 +7,8 @@ rm -rf ./feeds/packages/net/{geoview,shadowsocks-libev,chinadns-ng,mosdns}
 rm -rf ./feeds/luci/applications/luci-app-mosdns
 
 #克隆依赖插件
-git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
-git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+# git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/pwpage
+# git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
 
 
 #克隆的源码放在small文件夹
@@ -16,11 +16,11 @@ mkdir package/small
 pushd package/small
 
 #luci-app-nft-timecontrol
-git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
+# git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-timecontrol.git
 #adguardhome
-git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+# git clone -b 2024.09.05 --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
 #lucky
-git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
+# git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
 # #smartdns
 # git clone -b lede --depth 1 https://github.com/pymumu/luci-app-smartdns.git
 # git clone -b master --depth 1 https://github.com/pymumu/smartdns.git
@@ -29,13 +29,13 @@ git clone -b main --depth 1 https://github.com/gdy666/luci-app-lucky.git
 # #passwall
 # git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall.git
 #passwall2
-git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+# git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
 # #mosdns
-git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+# git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
 # #openclash
 # git clone -b master --depth 1 https://github.com/vernesong/OpenClash.git
 # OpenWrt-nikki
-git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
+# git clone --depth 1 https://github.com/nikkinikki-org/OpenWrt-nikki.git
 
 popd
 
