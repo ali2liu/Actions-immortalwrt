f5caea5b4f60529483906ac672d9bcf797b5bf41
ali2liu <ali2liu@gmail.com>
2026-02-03 07:51:32 +0800

    Update add_custom_feed.sh

--- scripts/add_custom_feed.sh
+++ scripts/add_custom_feed.sh
@@ -2,3 +2,4 @@
 
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
+echo "src-git rtp2httpd https://github.com/stackia/rtp2httpd.git;main" >> "feeds.conf.default"
