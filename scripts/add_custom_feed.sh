cff9f65c4d7ad6f04537d99d6872e4766281acec
ali2liu <ali2liu@gmail.com>
2026-04-01 17:13:55 +0800

    Update add_custom_feed.sh

--- scripts/add_custom_feed.sh
+++ scripts/add_custom_feed.sh
@@ -3,3 +3,4 @@
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 echo "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main" >> "feeds.conf.default"
 # echo "src-git rtp2httpd https://github.com/stackia/rtp2httpd.git;main" >> "feeds.conf.default"
+sed -i 's|src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-23.05|src-git luci https://github.com/coolsnowwolf/luci.git;openwrt-25.12|' feeds.conf.default
